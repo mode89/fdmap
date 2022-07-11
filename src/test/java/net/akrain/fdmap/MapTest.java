90cfb59a78129e250890ab5d64bc2e35b8acbfca
Andrey Krainyak <mode.andrew@gmail.com>
2022-07-11 23:37:46 +0800

    Running unit tests

---
+++ src/test/java/net/akrain/fdmap/MapTest.java
@@ -0,0 +1,12 @@
+package net.akrain.fdmap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MapTest {
+
+    @Test
+    void helloWorld() {
+        assertTrue(true);
+    }
+}
