db6e881e9458073156eb6fc4da8af52a1e1c61d7
Andrey Krainyak <mode.andrew@gmail.com>
2022-07-12 12:19:23 +0800

    Test Map's API

--- src/test/java/net/akrain/fdmap/MapTest.java
+++ src/test/java/net/akrain/fdmap/MapTest.java
@@ -185,4 +185,23 @@ public class MapTest {
         assertTrue(nodeGetEntry(c3, 0, 1, 2) == e2);
         assertTrue(nodeGetEntry(c3, 0, 1, 3) == e3);
     }
+
+    @Test
+    void map() {
+        final Map m1 = new Map(null);
+        final Map m2 = m1.assoc(1, "1");
+        final Map m3 = m2.assoc(1, "1");
+        final Map m4 = m2.assoc(1, "2");
+        final Map m5 = m1.dissoc(1);
+        final Map m6 = m4.dissoc(2);
+        final Map m7 = m2.dissoc(1);
+        assertNull(m1.get(1));
+        assertEquals("1", m2.get(1));
+        assertTrue(m2 == m3);
+        assertEquals("2", m4.get(1));
+        assertNull(m4.get(2));
+        assertTrue(m1 == m5);
+        assertTrue(m4 == m6);
+        assertNull(m7.get(1));
+    }
 }
