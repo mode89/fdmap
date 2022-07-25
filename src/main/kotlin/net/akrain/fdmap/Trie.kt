a8e66cbddfbf9ca2cccddd91185cec5768126f02
Andrey Krainyak <mode.andrew@gmail.com>
2022-07-25 15:07:49 +0800

    Port dissoc() to Kotlin

--- src/main/kotlin/net/akrain/fdmap/Trie.kt
+++ src/main/kotlin/net/akrain/fdmap/Trie.kt
@@ -13,7 +13,7 @@ data class ArrayNode(
 )
 
 data class CollisionNode(
-    val children: ArrayList<Entry>,
+    val children: List<Entry>,
     val keyHash: Int,
 )
 
@@ -118,6 +118,104 @@ fun getEntry(node: Any, shift: Int, keyHash: Int, key: Any): Entry? {
     }
 }
 
+fun dissoc(node: Any, shift: Int, keyHash: Int, key: Any): Any? {
+    return when (node) {
+        is ArrayNode -> dissocArrayNode(node, shift, keyHash, key)
+        is Entry -> dissocEntry(node, keyHash, key)
+        is CollisionNode -> dissocCollisionNode(node, keyHash, key)
+        else -> throw UnsupportedOperationException()
+    }
+}
+
+private fun dissocArrayNode(
+        node: ArrayNode,
+        shift: Int,
+        keyHash: Int,
+        key: Any): Any {
+    val childIndex = arrayIndex(shift, keyHash)
+    val child = node.children[childIndex]
+    return if (child != null) {
+        val newChild = dissoc(child, shift + 5, keyHash, key)
+        if (child == newChild) {
+            node
+        } else {
+            val newChildrenCount = if (newChild != null) {
+                node.childrenCount
+            } else {
+                node.childrenCount - 1
+            }
+
+            if (newChildrenCount == 0) {
+                throw UnsupportedOperationException(
+                    "If no children left, that means the ArrayNode had "
+                    + "only one entry under it, which isn't allowed")
+            } else {
+                // If only one child left and it isn't an ArrayNode,
+                // we should return this child, instead
+                val returnChild = if (newChildrenCount != 1) {
+                    null
+                } else {
+                    if (newChild != null) {
+                        if (newChild is ArrayNode) null else newChild
+                    } else {
+                        // Should always succeed, because ArrayNode must
+                        // have at least two entries under it
+                        val lastChild = node.children.first(
+                            { (it != null) and (it != child) })
+                        if (lastChild is ArrayNode) null else lastChild
+                    }
+                }
+
+                if (returnChild == null) {
+                    ArrayNode(
+                        node.children.immSet(childIndex, newChild),
+                        newChildrenCount,
+                        node.entryCount - 1)
+                } else {
+                    returnChild
+                }
+            }
+        }
+    } else {
+        node
+    }
+}
+
+private fun dissocEntry(
+        node: Entry,
+        keyHash: Int,
+        key: Any): Any? {
+    return if ((node.keyHash == keyHash) and (node.key == key)) {
+        null
+    } else {
+        node
+    }
+}
+
+private fun dissocCollisionNode(
+        node: CollisionNode,
+        keyHash: Int,
+        key: Any): Any {
+    return if (node.keyHash != keyHash) {
+        node
+    } else {
+        val childIndex = node.children.indexOfFirst { it.key == key }
+        if (childIndex == -1) {
+            node
+        } else {
+            if (node.children.size > 2) {
+                CollisionNode(
+                    node.children.immRemoveAt(childIndex),
+                    keyHash)
+            } else {
+                // Should always succeed, because CollisionNode must have
+                // at least two children
+                node.children.first { it.key != key }
+            }
+        }
+    }
+}
+
 internal fun makeArrayNode(node: Any, shift: Int): ArrayNode {
     val children = arrayOfNulls<Any>(32)
     val index = arrayIndex(shift, getKeyHash(node))
@@ -143,14 +241,20 @@ private fun <T> Array<T>.immSet(i: Int, x: T): Array<T> {
     return arr
 }
 
-private fun <T> ArrayList<T>.immAdd(x: T): ArrayList<T> {
+private fun <T> List<T>.immAdd(x: T): List<T> {
     val arr = ArrayList(this)
     arr.add(x)
     return arr
 }
 
-private fun <T> ArrayList<T>.immSet(i: Int, x: T): ArrayList<T> {
+private fun <T> List<T>.immSet(i: Int, x: T): List<T> {
     val arr = ArrayList(this)
     arr.set(i, x)
+    return arr
+}
+
+private fun <T> List<T>.immRemoveAt(i: Int): List<T> {
+    val arr = ArrayList(this)
+    arr.removeAt(i)
     return arr
 }
